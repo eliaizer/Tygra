06896ed72c1bcb621579014b3da056d15169da0b
eliaizer <goshha625@gmail.com>
2026-05-31 03:40:51 +0300

    Это маленький шаг для человечества, но огромный шаг для ленивого меня!

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+app = tk.Tk()
+app.title("Tygra - графический ffmpeg")
+
+app.geometry(f"{int(app.winfo_screenwidth()*0.35)}x{int(app.winfo_screenheight()*0.35)}")
+
+app.mainloop()
