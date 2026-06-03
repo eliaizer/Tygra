be4fab71154b6c2f35272fe52fba70390026481f
eliaizer <goshha625@gmail.com>
2026-06-03 04:16:30 +0300

    Modified: code app (main.py) and README.md Add: icon app

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tygra
 GUI for ffmpeg
+
+## Road-map
+- [x] App on CustomTkinter
+- [ ] Drag-and-Drop
+- [ ] ffmpeg integration
+  - [ ] Work
+  - [ ] View and update in real-time available features for work
+- [ ] Installer (for Windows and maybe for Linux)
+- [ ] CI/CD (for mu update and update ffmpeg)
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import tkinter as tk
+from PIL import Image
+from customtkinter import *
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
-app = tk.Tk()
+app = CTk()
+
 app.title("Tygra - графический ffmpeg")
+app.iconbitmap("res/circle.ico")
 
-app.geometry(f"{int(app.winfo_screenwidth()*0.35)}x{int(app.winfo_screenheight()*0.35)}")
+x = int(app.winfo_screenwidth()*0.35)
+y = int(app.winfo_screenheight()*0.35)
+app.geometry(f"{x}x{y}")
+
+logo = CTkImage(light_image=Image.open("res/circle.png"), size=(100, 100))
+CTkLabel(app, text="", image=logo).pack(fill="x", pady=10, padx=10)
+CTkLabel(app, text="Tygra - GUI конвертор на базе ffmpeg", font=('JetBrains Mono Bold', 20)).pack(fill="x", pady=10, padx=10)
+
+left = CTkFrame(app, width=(x*0.95)//2)
+right = CTkFrame(app, width=(x*0.95)//2)
+
+left.pack(side="left", fill="y", padx=10, pady=10)
+right.pack(side="right", fill="y", padx=10, pady=10)
 
 app.mainloop()
